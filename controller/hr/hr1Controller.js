fe140d967c6d9a66755aa47e1b3b4f6fd9748f17
ImRaffy14 <raffysolis123@gmail.com>
2025-03-05 01:57:09 +0800

    `Added getApprovedLeaves endpoint to hr1Controller and hr1Routes`

--- controller/hr/hr1Controller.js
+++ controller/hr/hr1Controller.js
@@ -42,7 +42,24 @@ const timeTracking = async (req, res) => {
     }
 }
 
+// GET APPROVED LEAVES
+const getApprovedLeaves = async (req, res) => {
+    const server = req.decoded.service
+    try {
+        const token = generateServiceToken(server);
+        const response = await axios.get(`${process.env.HR1_SERVICE_URL}/api/leave/get-approved-leaves`,{
+            headers: { Authorization: `Bearer ${token}` },
+        });
+        console.log(`[${getCurrentDateTime()}] ${server} Requested to HR 1 Service (Get Approved Leaves)`);
+        res.status(response.status).json(response.data);
+    } catch (error) {
+        res.status(error.response?.status || 500).json({ error: error.message });
+        console.error(`Error Hr 1: Server:${server} ${error.message}`)
+    }
+}
+
 module.exports = {
     getEmployeeRecords,
-    timeTracking
+    timeTracking,
+    getApprovedLeaves
 }
--- routes/hr1Routes.js
+++ routes/hr1Routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getEmployeeRecords, timeTracking } = require('../controller/hr/hr1Controller')
+const { getEmployeeRecords, timeTracking, getApprovedLeaves } = require('../controller/hr/hr1Controller')
 
 
 const router = express.Router()
@@ -9,5 +9,8 @@ router.get('/get-employee-records', getEmployeeRecords)
 
 // GET TIME TRACKING
 router.get('/get-time-tracking', timeTracking)
+
+// GET APPROVED LEAVES
+router.get('/get-approved-leaves', getApprovedLeaves)
 
 module.exports = router
