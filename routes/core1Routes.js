04fba3e270e04c666a2575d0ba104f6c40c13a3c
ImRaffy14 <raffysolis123@gmail.com>
2025-03-05 21:08:49 +0800

    "Update core1Routes.js to include receiveOrderInformation from core1Controller"

--- routes/core1Routes.js
+++ routes/core1Routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { getWorkOrders } = require('../controller/core/core1Controller')
+const { getWorkOrders, receiveOrderInformation } = require('../controller/core/core1Controller')
 
 const router = express.Router()
 
