ec3e7a3530f206148c755be624bda48d2dff251b
ImRaffy14 <raffysolis123@gmail.com>
2025-03-09 20:39:45 +0800

    `Added transferProducts function to logistic2Controller.js and corresponding route to logistic2Routes.js`

--- controller/logistic/logistic2Controller.js
+++ controller/logistic/logistic2Controller.js
@@ -26,6 +26,23 @@ const updatePurchaseOrder = async (req, res) => {
     }
 }
 
+// TRANSFER PRODUCTS
+const transferProducts = async (req, res) => {
+    const server = req.decoded.service
+    try {
+        const token = generateServiceToken(server);
+        const response = await axios.post(`${process.env.LOGISTIC2_SERVICE_URL}/api/finishedproducttransfers`, req.body, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+        console.log(`[${getCurrentDateTime()}] ${server} Requested to Logistic 2 Service (Transfer Products)`);
+        res.status(response.status).json(response.data);
+    } catch (error) {
+        res.status(error.response?.status || 500).json({ error: error.message });
+        console.error(`Error Logistic 2: Server:${server} ${error.message}`)
+    }
+}
+
 module.exports = {
     updatePurchaseOrder,
+    transferProducts
 }
--- routes/logistic2Routes.js
+++ routes/logistic2Routes.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const { updatePurchaseOrder } = require('../controller/logistic/logistic2Controller')
+const { updatePurchaseOrder, transferProducts } = require('../controller/logistic/logistic2Controller')
 
 const router = express.Router()
 
 // UPDATE PURCHASE ORDER FOR FINANCE
 router.post('/purchase-order-update', updatePurchaseOrder)
+
+// TRANSFER PRODUCTS
+router.post('/transfer-products', transferProducts)
 
 
 module.exports = router
